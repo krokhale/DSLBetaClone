3e60fa693ce0bbf3ebfee47391c6161e04882af9
Pushyami Kaveti <pushyamis.kaveti@gmail.com>
2013-07-01 18:50:31 -0400

    in between the signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+     #it is saved succesfully
+    else
+      @title= "Sign Up!"
+      render 'new'
+    end
+     
+  end
   def new
     @title = "Sign up!"
+    @user= User.new
   end
   
   def show
     @user = User.find(params[:id])
     @title = @user.name;
   end
+  
 end
