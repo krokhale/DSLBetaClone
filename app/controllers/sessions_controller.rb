2ffe7ca061ec9fdedb91deb0a72fa371274dc690
Pushyami Kaveti <pushyamis.kaveti@gmail.com>
2013-07-02 01:47:12 -0400

    in between sign and sign out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def create
+    user= User.authenticate(params[:session][:email],params[:session][:password])
+    if user.nil?
+      #error mesage and re-render sign in form
+      flash.now[:error]= "Invalid email/password combination"
+      @title = "Sign in"
+      render 'new' 
+    else
+      #sign in the user and open the show page of the user
+      sign_in user
+      redirect_to user
+    end
+  end
+  
+  def new
+    @title = "Sign In"
+  end
+  
+  def destroy
+  end
+  
+end
