63d8f205f2c07214c12fa962ce37c263acdac209
Pushyami Kaveti <pushyamis.kaveti@gmail.com>
2013-07-26 12:35:51 -0400

    comparing solutions fixed

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -3,7 +3,7 @@ class EvaluationsController < ApplicationController
   def check
      @lesson=Lesson.find(session[:lid])
      if params[:solution]
-       if @lesson.solution==params[:solution]
+       if @lesson.solution.strip == params[:solution].strip
          flash[:success]="solution is correct !! Go to next lesson"
        else
          flash[:error] ="Solution is incorrect.try again"  
