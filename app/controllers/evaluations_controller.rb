ef85bfcedf70d8dd5bfead9c59f1a53ea81b0d1b
Pushyami Kaveti <pushyamis.kaveti@gmail.com>
2013-07-30 19:56:08 -0400

    updated evaluation system

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,16 +1,74 @@
 class EvaluationsController < ApplicationController
 
+ # Check and display the results of the evaluation of the solution
   def check
      @lesson=Lesson.find(session[:lid])
      if params[:solution]
-       if @lesson.solution.strip == params[:solution].strip
-         flash[:success]="solution is correct !! Go to next lesson"
+       eval = solution_correct?
+         if eval == true
+           flash[:success]="solution is correct !! Go to next lesson"
+         else
+           mesg_hash=@lesson.messages
+           str=""
+           eval.each do |e|
+             if mesg_hash.has_key?(e)
+              str = str+"\n"+mesg_hash[e]
+             else
+              str = str+"\n"+"check the term #{e}" 
+             end
+           end
+           flash[:error] ="Solution is incorrect.#{str}"  
+         end
        else
-         flash[:error] ="Solution is incorrect.try again"  
-       end
-     else
-       flash[:notice]="empty"
-     end 
+         flash[:notice]="empty"
+       end 
      redirect_to @lesson     
   end
+  
+private
+
+# see if the solution given by the user is correct
+  def solution_correct?
+    current = params[:solution].strip
+    current.gsub!(/\/\*[\w\s]*\*\//,"") 
+    current.gsub!(/--.*\n/,"")
+    existing =  format_str(@lesson.solution.strip)
+    current = format_str(current.strip)
+    if existing == current
+      return true
+    else
+      existing_arr = existing.split
+      current_arr = current.split
+      len= existing_arr.length
+      err =[]
+      j=0      
+      for i in 0..len
+        if existing_arr[i]!=current_arr[i]
+          err[j]= existing_arr[i]
+          j=j+1  
+        end
+      end
+      return err
+    end   
+  end
+  
+ #method for the preprocessing of the string before comparision
+  def format_str(str)
+    str.gsub!(/ *[\(\),=\*;] */) do |n|
+      if (/\(/ =~ n) != nil
+        " ( "
+      elsif (/\)/ =~ n) != nil
+        " ) "
+      elsif (/,/ =~ n) != nil  
+        " , "
+      elsif (/=/ =~ n) != nil
+        " = "
+      elsif (/;/ =~ n) != nil
+        " ; "
+      else
+        " * "  
+      end
+    end
+  end
+  
 end
